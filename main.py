7c7efe7fe541921859d0fd6a4f65eab6c6b3b3d5
Ignacy Bojarski <ignacy.bojarski@gmail.com>
2025-02-02 23:30:14 +0100

    Timer - notes

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from timer import Timer as T
 def main():
     m = input("Enter the length of one study session (min): ")
     b = input("Enter the length of the break (min): ")
-    t = T(int(m), int(b))
-    t.studytimer()
+    t = T(int(m), int(b)) #create timer object
+    t.studytimer() #start timer
 
 if __name__ == "__main__":
     main()
