923ca4b1e5d6f39116e11e4c6f3c97e0694bd803
Ignacy Bojarski <ignacy.bojarski@gmail.com>
2025-02-10 20:50:12 +0100

    fix replacing timer

--- timer.py
+++ timer.py
@@ -1,31 +1,36 @@
 import time as t
 import datetime as dt
+import sys
 
 class Timer():
     def __init__(self, m, b):
-        self.m = m
-        self.b = b
+        self.studyTime = m*60 - 1
+        self.breakTime = b*60 - 1
 
     def studytimer(self):
-        totalSeconds = self.m * 60 - 1
-        print("\rTime to focus!", end="")
+        totalSeconds = self.studyTime
+        sys.stdout.write("\rTime to focus!", end="")
+        sys.stdout.flush()
         t.sleep(1)
         while totalSeconds >= 0:
             timeleft = dt.timedelta(seconds = totalSeconds)
             display = f"\r{timeleft}"
-            print(display, end="")
+            sys.stdout.write(display, end="")
+            sys.stdout.flush()
             t.sleep(1)
             totalSeconds -= 1
         self.breaktimer()
 
     def breaktimer(self):
-        totalSeconds = self.b*60 - 1
-        print("\rBreak!", end="")
+        totalSeconds = self.breakTime
+        sys.stdout.write("\rBreak!", end="")
+        sys.stdout.flush()
         t.sleep(1)
         while totalSeconds >= 0:
             timeleft = dt.timedelta(seconds=totalSeconds)
             display = f"\r{timeleft}"
-            print(display, end="")
+            sys.stdout.write(display, end="")
+            sys.stdout.flush()
             t.sleep(1)
             totalSeconds -= 1
         self.studytimer()
